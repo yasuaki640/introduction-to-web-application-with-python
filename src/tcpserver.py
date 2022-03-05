610fea3f0e48de8b82c494dd496f80c4739fc557
yasuaki640 <yasuaki640@gmail.com>
2022-03-05 16:55:23 +0900

    案の定詰まりまくりました

---
+++ src/tcpserver.py
@@ -0,0 +1,33 @@
+import socket
+
+
+class TCPServer:
+
+    def serve(self):
+
+        print("=== サーバーを起動します ===")
+
+        try:
+            server_socket = socket.socket()
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            server_socket.bind((socket.gethostname(), 8080))
+            server_socket.listen(10)
+
+            print("=== クライアントからの接続を待ちます ===")
+            (client_socket, address) = server_socket.accept()
+            print(f"=== クライアントとの接続が完了しました remote_address: {address} ===")
+
+            request = client_socket.recv(4096)
+
+            with open("server_recv.txt", "wb") as f:
+                f.write(request)
+
+            client_socket.close()
+
+        finally:
+            print("=== サーバーを停止します。 ===")
+
+
+if __name__ == '__main__':
+    server = TCPServer()
+    server.serve()
