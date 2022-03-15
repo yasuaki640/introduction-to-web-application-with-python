288e40b7ff66c91e7b56a152577740dac282e526
yasuaki640 <yasuaki640@gmail.com>
2022-03-16 08:23:52 +0900

    funk

--- src/webserver.py
+++ src/webserver.py
@@ -1,11 +1,20 @@
 import os
 import socket
+import traceback
 from datetime import datetime
 
 
 class WebServer:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+    MIME_TYPES = {
+        "html": "text/html",
+        "css": "text/css",
+        "png": "image/png",
+        "jpg": "image/jpg",
+        "gif": "image/gif",
+    }
 
     def serve(self):
         print("=== サーバーを起動します ===")
@@ -17,47 +26,60 @@ class WebServer:
             server_socket.bind((socket.gethostname(), 8080))
             server_socket.listen(10)
 
-            print("=== クライアントからの接続を待ちます ===")
-            (client_socket, address) = server_socket.accept()
-            print(f"=== クライアントとの接続が完了しました remote_address: {address} ===")
+            while True:
+                print("=== クライアントからの接続を待ちます ===")
+                (client_socket, address) = server_socket.accept()
+                print(f"=== クライアントとの接続が完了しました remote_address: {address} ===")
 
-            request = client_socket.recv(4096)
+                try:
+                    request = client_socket.recv(4096)
 
-            with open("server_recv.txt", "wb") as f:
-                f.write(request)
+                    with open("server_recv.txt", "wb") as f:
+                        f.write(request)
 
-            request_line, remain = request.split(b"\r\n", maxsplit=1)
-            request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
+                    request_line, remain = request.split(b"\r\n", maxsplit=1)
+                    request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
 
-            method, path, http_version = request_line.decode().split(" ")
+                    method, path, http_version = request_line.decode().split(" ")
 
-            # pathの先頭の/を削除し、相対パスにしておく
-            relative_path = path.lstrip("/")
+                    relative_path = path.lstrip("/")
 
-            static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
+                    static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
 
-            try:
-                with open(static_file_path, "rb") as f:
-                    response_body = f.read()
+                    try:
+                        with open(static_file_path, "rb") as f:
+                            response_body = f.read()
 
-                response_line = "HTTP/1.1 200 OK\r\n"
+                        response_line = "HTTP/1.1 200 OK\r\n"
 
-            except OSError:
-                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-                response_line = "HTTP/1.1 404 Not Found\r\n"
+                    except OSError:
+                        response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+                        response_line = "HTTP/1.1 404 Not Found\r\n"
 
-            response_header = ""
-            response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-            response_header += "Host: HenaServer/0.1\r\n"
-            response_header += f"Content-Length: {len(response_body)}\r\n"
-            response_header += "Connection: Close\r\n"
-            response_header += "Content-Type: text/html\r\n"
+                    if "." in path:
+                        ext = path.rsplit(".", maxsplit=1)[-1]
+                    else:
+                        ext = ""
 
-            response = (response_line + response_header + "\r\n").encode() + response_body
+                    content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
 
-            client_socket.send(response)
+                    response_header = ""
+                    response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+                    response_header += "Host: HenaServer/0.1\r\n"
+                    response_header += f"Content-Length: {len(response_body)}\r\n"
+                    response_header += "Connection: Close\r\n"
+                    response_header += f"Content-Type: {content_type}\r\n"
 
-            client_socket.close()
+                    response = (response_line + response_header + "\r\n").encode() + response_body
+
+                    client_socket.send(response)
+
+                except Exception:
+                    print("=== リクエストの処理中にエラーが発生しました ===")
+                    traceback.print_exc()
+
+                finally:
+                    client_socket.close()
 
         finally:
             print("=== サーバーを停止します。 ===")
