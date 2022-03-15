0844da68362064d6dac4147b392548df6da9015c
yasuaki640 <yasuaki640@gmail.com>
2022-03-15 09:07:18 +0900

    established ultimate server

--- src/webserver.py
+++ src/webserver.py
@@ -1,8 +1,11 @@
+import os
 import socket
 from datetime import datetime
 
 
 class WebServer:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
     def serve(self):
         print("=== サーバーを起動します ===")
@@ -10,6 +13,7 @@ class WebServer:
         try:
             server_socket = socket.socket()
             server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
             server_socket.bind((socket.gethostname(), 8080))
             server_socket.listen(10)
 
@@ -22,18 +26,29 @@ class WebServer:
             with open("server_recv.txt", "wb") as f:
                 f.write(request)
 
-            response_body = "<html><body><h1>It works!</h1></body></html>"
+            request_line, remain = request.split(b"\r\n", maxsplit=1)
+            request_header, request_body = remain.split(b"\r\n\r\n", maxsplit=1)
+
+            method, path, http_version = request_line.decode().split(" ")
+
+            # pathの先頭の/を削除し、相対パスにしておく
+            relative_path = path.lstrip("/")
+
+            static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
+
+            with open(static_file_path, "rb") as f:
+                response_body = f.read()
 
             response_line = "HTTP/1.1 200 OK\r\n"
 
             response_header = ""
             response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
             response_header += "Host: HenaServer/0.1\r\n"
-            response_header += f"Content-Length: {len(response_body.encode())}\r\n"
+            response_header += f"Content-Length: {len(response_body)}\r\n"
             response_header += "Connection: Close\r\n"
             response_header += "Content-Type: text/html\r\n"
 
-            response = (response_line + response_header + "\r\n" + response_body).encode()
+            response = (response_line + response_header + "\r\n").encode() + response_body
 
             client_socket.send(response)
 
@@ -43,6 +58,6 @@ class WebServer:
             print("=== サーバーを停止します。 ===")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     server = WebServer()
     server.serve()
