b9d501c5ee7eec1d92f50eeddd1d76a8c6c765d5
yasuaki640 <yasuaki640@gmail.com>
2022-03-17 08:59:51 +0900

    asdfl;akerjgorg

--- src/workerthread.py
+++ src/workerthread.py
@@ -2,6 +2,7 @@ import os
 import re
 import textwrap
 import traceback
+import urllib.parse
 from datetime import datetime
 from pprint import pformat
 from socket import socket
@@ -76,6 +77,30 @@ class WorkerThread(Thread):
                 response_body = textwrap.dedent(html).encode()
                 content_type = "text/html; charset=UTF-8"
                 response_line = "HTTP/1.1 200 OK\r\n"
+
+            elif path == "/parameters":
+                if method == "GET":
+                    response_body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
+                    content_type = "text/html; charset=UTF-8"
+                    response_line = "HTTP/1.1 405 Method Not Allowed\r\n"
+
+                elif method == "POST":
+                    post_params = urllib.parse.parse_qs(request_body.decode())
+                    html = f"""\
+                        <html>
+                        <body>
+                            <h1>Parameters:</h1>
+                            <pre>{pformat(post_params)}</pre>                        
+                        </body>
+                        </html>
+                    """
+                    response_body = textwrap.dedent(html).encode()
+
+                    # Content-Typeを指定
+                    content_type = "text/html; charset=UTF-8"
+
+                    # レスポンスラインを生成
+                    response_line = "HTTP/1.1 200 OK\r\n"
 
             else:
                 try:
