34f4161a930e68cafa942a0e78bf951020a602ff
yasuaki640 <yasuaki640@gmail.com>
2022-03-16 09:18:24 +0900

    now

--- src/workerthread.py
+++ src/workerthread.py
@@ -1,13 +1,13 @@
 import os
+import textwrap
 import traceback
 from datetime import datetime
 from socket import socket
 from threading import Thread
-from typing import Tuple
+from typing import Tuple, Optional
 
 
 class WorkerThread(Thread):
-
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
@@ -39,16 +39,40 @@ class WorkerThread(Thread):
 
             method, path, http_version, request_header, request_body = self.parse_http_request(request)
 
-            try:
-                response_body = self.get_static_file_content(path)
+            response_body: bytes
+            content_type: Optional[str]
+            response_line: str
+
+            if path == "/now":
+                html = f"""\
+                    <html>
+                    <body>
+                        <h1>Now: {datetime.now()}</h1>
+                    </body>
+                    </html>
+                """
+                response_body = textwrap.dedent(html).encode()
+
+                content_type = "text/html"
 
                 response_line = "HTTP/1.1 200 OK\r\n"
 
-            except OSError:
-                response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
-                response_line = "HTTP/1.1 404 Not Found\r\n"
+            else:
+                try:
+                    response_body = self.get_static_file_content(path)
 
-            response_header = self.build_response_header(path, response_body)
+                    content_type = None
+
+                    response_line = "HTTP/1.1 200 OK\r\n"
+
+                except OSError:
+                    traceback.print_exc()
+
+                    response_body = b"<html><body><h1>404 Not Found</h1></body></html>"
+                    content_type = "text/html"
+                    response_line = "HTTP/1.1 404 Not Found\r\n"
+
+            response_header = self.build_response_header(path, response_body, content_type)
 
             response = (response_line + response_header + "\r\n").encode() + response_body
 
@@ -86,22 +110,24 @@ class WorkerThread(Thread):
         """
 
         relative_path = path.lstrip("/")
+
         static_file_path = os.path.join(self.STATIC_ROOT, relative_path)
 
         with open(static_file_path, "rb") as f:
             return f.read()
 
-    def build_response_header(self, path: str, response_body: bytes) -> str:
+    def build_response_header(self, path: str, response_body: bytes, content_type: Optional[str]) -> str:
         """
         レスポンスヘッダーを構築する
         """
 
-        if "." in path:
-            ext = path.rsplit(".", maxsplit=1)[-1]
-        else:
-            ext = ""
+        if content_type is None:
+            if "." in path:
+                ext = path.rsplit(".", maxsplit=1)[-1]
+            else:
+                ext = ""
 
-        content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
+            content_type = self.MIME_TYPES.get(ext, "application/octet-stream")
 
         response_header = ""
         response_header += f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
