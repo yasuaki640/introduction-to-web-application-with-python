2cc7271ac5d6409437529420f5d1893467fa9699
yasuaki640 <yasuaki640@gmail.com>
2022-03-17 09:21:17 +0900

    aoeirger

--- src/workerthread.py
+++ src/workerthread.py
@@ -1,13 +1,12 @@
 import os
 import re
-import textwrap
 import traceback
-import urllib.parse
 from datetime import datetime
-from pprint import pformat
 from socket import socket
 from threading import Thread
 from typing import Tuple, Optional
+
+import views
 
 
 class WorkerThread(Thread):
@@ -47,67 +46,20 @@ class WorkerThread(Thread):
             response_line: str
 
             if path == "/now":
-                html = f"""\
-                    <html>
-                    <body>
-                        <h1>Now: {datetime.now()}</h1>
-                    </body>
-                    </html>
-                """
-                response_body = textwrap.dedent(html).encode()
-                content_type = "text/html; charset=UTF-8"
-                response_line = "HTTP/1.1 200 OK\r\n"
+                response_body, content_type, response_line = views.now()
 
             elif path == "/show_request":
-                html = f"""\
-                    <html>
-                    <body>
-                        <h1>Request Line:</h1>
-                        <p>
-                            {method} {path} {http_version}
-                        </p>
-                        <h1>Headers:</h1>
-                        <pre>{pformat(request_header)}</pre>
-                        <h1>Body:</h1>
-                        <pre>{request_body.decode("utf-8", "ignore")}</pre>
-
-                    </body>
-                    </html>
-                """
-                response_body = textwrap.dedent(html).encode()
-                content_type = "text/html; charset=UTF-8"
-                response_line = "HTTP/1.1 200 OK\r\n"
+                response_body, content_type, response_line = views.show_request(
+                    method, path, http_version, request_header, request_body
+                )
 
             elif path == "/parameters":
-                if method == "GET":
-                    response_body = b"<html><body><h1>405 Method Not Allowed</h1></body></html>"
-                    content_type = "text/html; charset=UTF-8"
-                    response_line = "HTTP/1.1 405 Method Not Allowed\r\n"
-
-                elif method == "POST":
-                    post_params = urllib.parse.parse_qs(request_body.decode())
-                    html = f"""\
-                        <html>
-                        <body>
-                            <h1>Parameters:</h1>
-                            <pre>{pformat(post_params)}</pre>                        
-                        </body>
-                        </html>
-                    """
-                    response_body = textwrap.dedent(html).encode()
-
-                    # Content-Typeを指定
-                    content_type = "text/html; charset=UTF-8"
-
-                    # レスポンスラインを生成
-                    response_line = "HTTP/1.1 200 OK\r\n"
+                response_body, content_type, response_line = views.parameters(method, request_body)
 
             else:
                 try:
                     response_body = self.get_static_file_content(path)
-
                     content_type = None
-
                     response_line = "HTTP/1.1 200 OK\r\n"
 
                 except OSError:
