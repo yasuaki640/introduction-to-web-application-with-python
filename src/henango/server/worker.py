3dac8e05955b58fe464542fbb4ea0d43995e5ed6
yasuaki640 <yasuaki640@gmail.com>
2022-03-23 09:21:56 +0900

    refactor

--- src/henango/server/worker.py
+++ src/henango/server/worker.py
@@ -2,15 +2,14 @@ import os
 import re
 import traceback
 from datetime import datetime
-from re import Match
 from socket import socket
 from threading import Thread
-from typing import Tuple, Optional
+from typing import Tuple
 
 import settings
 from henango.http.request import HTTPRequest
 from henango.http.response import HTTPResponse
-from urls import URL_VIEW
+from urls import url_patterns
 
 
 class Worker(Thread):
@@ -47,10 +46,11 @@ class Worker(Thread):
 
             request = self.parse_http_request(request_bytes)
 
-            for url_pattern, view in URL_VIEW.items():
-                match = self.url_match(url_pattern, request.path)
+            for url_pattern in url_patterns:
+                match = url_pattern.match(request.path)
                 if match:
                     request.params.update(match.groupdict())
+                    view = url_pattern.view
                     response = view(request)
                     break
 
@@ -139,7 +139,3 @@ class Worker(Thread):
         response_header += f"Content-Type: {response.content_type}\r\n"
 
         return response_header
-
-    def url_match(self, url_pattern: str, path: str) -> Optional[Match]:
-        re_pattern = re.sub(r"<(.+?)>", r"(?P<\1>[^/]+)", url_pattern)
-        return re.match(re_pattern, path)
--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,10 @@
 import views
+from henango.urls.pattern import URLPattern
 
-URL_VIEW = {
-    "/now": views.now,
-    "/show_request": views.show_request,
-    "/parameters": views.parameters,
-    "/user/<user_id>/profile": views.user_profile,
-}
+# pathとview関数の対応
+url_patterns = [
+    URLPattern("/now", views.now),
+    URLPattern("/show_request", views.show_request),
+    URLPattern("/parameters", views.parameters),
+    URLPattern("/user/<user_id>/profile", views.user_profile),
+]
