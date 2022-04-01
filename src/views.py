2db4d5336c7342446eae5de00ec476f160410486
yasuaki640 <yasuaki640@gmail.com>
2022-04-01 09:29:23 +0900

    a;oweifgjrb@j

--- src/views.py
+++ src/views.py
@@ -5,17 +5,17 @@ from pprint import pformat
 
 from henango.http.request import HTTPRequest
 from henango.http.response import HTTPResponse
+from henango.template.renderer import render
 
 
 def now(request: HTTPRequest) -> HTTPResponse:
     """
     現在時刻を表示するHTMLを生成する
     """
-    with open("./templates/now.html") as f:
-        template = f.read()
-        html = template.format(now=datetime.now())
+    context = {"now": datetime.now()}
+    html = render("./templates/now.html", context)
 
-    body = textwrap.dedent(html).encode()
+    body = html.encode()
     content_type = "text/html; charset=UTF-8"
 
     return HTTPResponse(body=body, content_type=content_type, status_code=200)
