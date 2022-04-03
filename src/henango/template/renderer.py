673bb302a35ea77e97f529d641f90c5fcfe36079
yasuaki640 <yasuaki640@gmail.com>
2022-04-03 17:24:35 +0900

    a;oweifgjrb@j

--- src/henango/template/renderer.py
+++ src/henango/template/renderer.py
@@ -1,4 +1,11 @@
-def render(template_path: str, context: dict):
+import os
+
+import settings
+
+
+def render(template_name: str, context: dict):
+    template_path = os.path.join(settings.TEMPLATES_DIR, template_name)
+
     with open(template_path) as f:
         template = f.read()
 
--- src/settings.py
+++ src/settings.py
@@ -3,3 +3,5 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
